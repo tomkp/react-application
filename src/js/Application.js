f84ef7185f0c57084c6aa32341b83e8614a25a2d
tomkp <tom@tomkp.com>
2015-03-10 21:01:27 +0000

    tab pane demo

--- src/js/Application.js
+++ src/js/Application.js
@@ -63,7 +63,7 @@ let Application = React.createClass({
                             <Flex className="content">
                                 <SplitPane orientation="horizontal" minSize="420">
 
-                                    <TabPane orientation="vertical" className="my-tabs">
+                                    <TabPane orientation="horizontal" className="my-tabs">
                                         <Tab id="A" name="A">some stuff</Tab>
                                         <Tab id="B" name="B" active="true">whatevs</Tab>
                                         <Tab id="C" name="C">innit</Tab>
